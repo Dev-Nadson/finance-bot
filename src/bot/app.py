9107abbefac9a17f3854452ccd7750c83ce6b6d9
CodeHPedro <code.pedrohenrique@gmail.com>
2026-03-16 22:58:08 -0300

    incrementação da função show_menu

--- src/bot/app.py
+++ src/bot/app.py
@@ -3,3 +3,7 @@ from bot.commands import v1
 
 bot.message_handler(commands=["start"])(v1.send_welcome)
 bot.message_handler(commands=["chart"])(v1.send_chart)
+bot.message_handler(commands=["menu"])(lambda msg: menu.show_menu(msg, bot))
+bot.callback_query_handler(func=lambda call: True )
+    def callback_query(call): 
+        menu.handle_callback(call, bot)
--- src/bot/commands/v1/__init__.py
+++ src/bot/commands/v1/__init__.py
@@ -1,2 +1,3 @@
 from .start_help import send_welcome
 from .send_chart import send_chart
+from .show_menu import show_menu, handle_callback
