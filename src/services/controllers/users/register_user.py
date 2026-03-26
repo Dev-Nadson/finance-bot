7f7765f37fe01a36a311c7f16a19df833a9fa5a4
Dev-Nadson <nadson.pros@gmail.com>
2026-03-26 14:50:00 -0300

    fix: register user updated

--- src/services/controllers/users/register_user.py
+++ src/services/controllers/users/register_user.py
@@ -6,11 +6,12 @@ async def register_user(user_name: str, telegram_id: int):
     user = User(name=user_name, telegram_id=telegram_id)
 
     async with get_session() as session:
-        user_exists = await session.execute(select(User).filter_by(telegram_id=telegram_id))
+        user_exists = (await session.execute(select(User).filter_by(telegram_id=telegram_id))).scalar_one_or_none()
         print(user_exists)
-        
-        if user_exists is None: #essa verificação tá quebrada e o print retorna: <sqlalchemy.engine.result.ChunkedIteratorResult object at 0x71ac03302c50>
+
+        if user_exists is not None: 
+            print(user_exists.to_dict())
+            return 409 
+        else:
             session.add(user)
             return 201
-        else:
-            return 409 
