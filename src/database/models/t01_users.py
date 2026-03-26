19d62ef481129f33d8055eea9e9ad631d53cf60c
Dev-Nadson <nadson.pros@gmail.com>
2026-03-26 14:50:14 -0300

    feat: to_dict added

--- src/database/models/t01_users.py
+++ src/database/models/t01_users.py
@@ -10,6 +10,15 @@ class User(Base):
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime)
 
+    def to_dict(self):
+        return {
+            "id": self.user_id,
+            "telegram_id": self.telegram_id,
+            "name": self.name,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     print("\033[32mUsers table created successfully.\033[0m")  # os números são para deixar verde
