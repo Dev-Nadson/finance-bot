75faf69d7e170cc7da54867aaef1d813fcc6073d
Dev-Nadson <nadson.pros@gmail.com>
2026-03-30 19:58:02 -0300

    feat: enhance create_account_repository with user and account existence checks

--- src/services/repositories/accounts/create_account.py
+++ src/services/repositories/accounts/create_account.py
@@ -1,22 +1,33 @@
 from sqlalchemy import select
 from database.models.db_config import get_session   
+from database.models.t01_users import User
 from database.models.t02_accounts import Account
+from database.models.t03_users_accounts import UserAccounts
+
 
 async def create_account_repository(account_name: str, user_id: int):
-    # Verificar se o usuário existe
-    # Verificar se a conta já existe para o usuário
-    # Se não existir, criar a conta e associar ao usuário
-    # pensar se ele cria a conta manuakmente ou se é criado automaticamente quando o usuário é criado
-
-    account = Account(name=account_name, user_id=user_id)
-
     async with get_session() as session:
-        pass
-        # account_exists = (await session.execute(select(Account).filter_by(name=account_name, user_id=user_id))).scalar_one_or_none()
+        user_exists = (await session.execute(select(User).filter_by(user_id=user_id))).scalar_one_or_none()
+        if user_exists is None:
+            print(f"User with ID {user_id} does not exist.") #dev
+            return 404
 
-        # if account_exists is not None: 
-        #     print(account_exists.to_dict()) #dev
-        #     return 409 
-        # else:
-        #     session.add(account)
-        #     return 201
+        account_exists = (await session.execute(select(UserAccounts).filter_by(user_id=user_id))).scalar_one_or_none()
+        if account_exists is not None: 
+            print(account_exists.to_dict()) #dev
+            return 409
+        
+        account = Account(name=account_name, user_id=user_id)
+        session.add(account)
+        await session.flush()
+    
+        user_account = UserAccounts(user_id=user_id, account_id=account.account_id)
+        session.add(user_account)
+        await session.flush()
+    
+        user_account_exists = (await session.execute(select(UserAccounts).filter_by(user_id=user_id, account_id=account.account_id))).scalar_one_or_none()
+        if user_account_exists is not None: 
+            print(user_account_exists.to_dict()) #dev
+            return 500
+
+        return 201 # user_account_exists.todict()
