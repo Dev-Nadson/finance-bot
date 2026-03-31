d10bc734bf97f23ded0620013b06d6e781d40643
CodeHPedro <code.pedrohenrique@gmail.com>
2026-03-31 11:21:38 -0300

    updated create incomes and expenses

--- src/services/repositories/expenses/create_expenses.py
+++ src/services/repositories/expenses/create_expenses.py
@@ -1,9 +1,30 @@
 from sqlalchemy import select
 from database.models.db_config import get_session
+from database.models.t01_users import User
+from database.models.t02_accounts import Account
 from database.models.t04_expenses import Expenses
 
-async def creat_expenses_repository(account_id:int, value: float, type: str, category: str, description: str):
+async def create_expenses_repository(account_id:int, value: float, type: str, category: str, description: str, telegram_id: str):
     async with get_session() as session:
-        expenses = Expenses(account_id = account_id, value = value, type = type, category = category, description = description)
-        session.add(expenses)
-        return 201
+        account_exists = (await session.execute(select(Account).filter_by(account_id=account_id))).scalar_one_or_none()
+        user_exists = (await session.execute(select(User).filter_by(telegram_id=telegram_id))).scalar_one_or_none()
+
+        if account_exists is None:
+            print(f'Erro: Conta não encontrada') # to_dict
+            return 404
+        if user_exists is None:
+            print(f'Usuario não encontrado') # to_dict
+            return 404
+        if account_exists.user_id != user_exists.user_id: # verifica se a conta é do usuario
+            print(f'Segurança: Usuário {telegram_id} tentou acessar conta de terceiros')
+            return 403
+        
+        try:
+            expenses = Expenses(account_id = account_id, value = value, type = type, category = category, description = description)
+            session.add(expenses)
+            await session.commit()
+            return 201
+        except Exception as e:
+            await session.rollback()
+            print(f'Erro Critico no banco: {e}')
+            return 500
--- src/services/repositories/incomes/create_incomes.py
+++ src/services/repositories/incomes/create_incomes.py
@@ -1,8 +1,29 @@
 from sqlalchemy import select
 from database.models.db_config import get_session
+from database.models.t01_users import User
+from database.models.t02_accounts import Account
 from database.models.t05_incomes import Incomes
 
-async def creat_incomes_repository(account_id:int, value: float, type: str, category: str, description: str):
+async def create_incomes_repository(incomes_id:int, value: float, type: str, category: str, description: str,account_id: int, telegram_id: str):
     async with get_session() as session:
-        incomes = Incomes(account_id = account_id, value = value, type = type, category = category, description = description)
-        session.add(incomes)
+        account_exists = (await session.execute(select(Account).filter_by(account_id=account_id))).scalar_one_or_none()
+        user_exists = (await session.execute(select(User).filter_by(telegram_id=telegram_id))).scalar_one_or_none()
+
+        if account_exists is None:
+            print(f'Erro: Conta não encontrada')
+            return 404
+        if user_exists is None:
+            print(f'Usuario não encontrado') 
+            return 404
+        if account_exists.user_id != user_exists.user_id:
+            print(f'Segurança: Usuário {telegram_id} tentou acessar conta de terceiros')
+            return 403
+        try:
+            incomes = Incomes(account_id = account_id, value = value, category = category, type = type, description = description)
+            session.add(incomes)
+            await session.commit()
+            return 201
+        except Exception as e:
+            await session.rollback()
+            print(f'Erro Critico no banco: {e}')
+            return 500
